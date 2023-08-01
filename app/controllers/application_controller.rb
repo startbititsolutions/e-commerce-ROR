705324caa99f972b9cf3f28374bc1feebff5e749
adityaru7 <adityajain@startbitsolutions.com>
2023-08-01 16:56:30 +0530

    customer log in approach

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,16 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    if resource.has_role?(:admin)
+    if resource.is_a?(User) && resource.has_role?(:admin)
       admin_root_path
+    elsif resource.is_a?(Customer)
+      
+     root_path
     else
-     
       root_path
     end
   end
-    
-  def after_customer_sign_in_path_for(resource)
-    # Define the logic specific to customer sign in here
-    root_path
-  end
+  
+ 
     protected
     
     def after_sign_out_path_for(resource_or_scope)
