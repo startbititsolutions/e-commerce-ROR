fde1521302e4bf55d4cf653f87d4c821c9a3f072
adityaru7 <adityajain@startbitsolutions.com>
2023-08-29 19:23:50 +0530

    Add env variable and stripe association

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,5 @@
 class Order < ApplicationRecord
     belongs_to  :customer
     has_many :line_items, dependent: :destroy
-    def sub_total
-        sum = 0
-        self.line_items.each do |line_item|
-          sum+= line_item.total_price
-        end
-        return sum
-      end
+   
 end
--- config/application.rb
+++ config/application.rb
@@ -13,13 +13,14 @@ module Filterbaseproduct
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.active_storage.replace_on_assign_to_many = false
+    config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # config.eager_load_paths << Rails.root.join("app/services")
    
   end
 end
