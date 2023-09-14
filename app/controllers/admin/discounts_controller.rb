322d2111fe4fc6d3a3ef0e4f9ddca2d5363a588d
Adityajainstartbit <adityajain@startbitsolutions.com>
2023-09-14 11:44:01 +0530

    my first commit

--- app/controllers/admin/discounts_controller.rb
+++ app/controllers/admin/discounts_controller.rb
@@ -55,7 +55,7 @@ module Admin
     @discount.destroy
 
     respond_to do |format|
-      format.html { redirect_to discounts_url, notice: "Discount was successfully destroyed." }
+      format.html { redirect_to admin_discounts_path, notice: "Discount was successfully destroyed." }
       format.json { head :no_content }
     end
   end
